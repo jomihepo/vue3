f9b215bcf2155dd7c4459e2901e97d78102f4536
Jose Miguel <jm.hernandez@iprisma.es>
2021-05-24 10:44:20 +0200

    - Configurando eslint y prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ["plugin:vue/essential", "standard", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["vue"],
+  rules: {}
+}
